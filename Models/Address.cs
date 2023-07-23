f928712ef1161fe70ab36ff578f1dd4241a6f17a
santosh maurya <santosh98maurya@hotmail.com>
2023-07-23 07:43:53 +0530

    comment

--- Models/Address.cs
+++ Models/Address.cs
@@ -6,6 +6,9 @@ namespace Models
 {
     public class Address
     {
+        /// <summary>
+        /// test
+        /// </summary>
         public string? Village { get; set; }
         public string? Taluka { get; set; }
         public string? City { get; set; }
